5377f23e518b38d58ad4082da205f5b945bbe27b
postmodern <postmodern.mod3@gmail.com>
2009-08-10 21:04:37 -0700

    Added a YARD task.

--- Rakefile
+++ Rakefile
@@ -4,12 +4,14 @@ require 'rubygems'
 require 'hoe'
 require 'hoe/signing'
 require './tasks/spec.rb'
+require './tasks/yard.rb'
 require './lib/contextify/version.rb'
 
 Hoe.spec('contextify') do
   self.rubyforge_name = 'contextify'
   self.developer('Postmodern', 'postmodern.mod3@gmail.com')
   self.remote_rdoc_dir = ''
+  self.extra_deps = [['yard', '>=0.2.3.4']]
 end
 
 # vim: syntax=Ruby
---
+++ tasks/yard.rb
@@ -0,0 +1,19 @@
+require 'yard'
+
+lib_dir = File.expand_path(File.join(File.dirname(__FILE__),'..','lib'))
+unless $LOAD_PATH.include?(lib_dir)
+  $LOAD_PATH << lib_dir
+end
+
+require 'contextify/yard/handlers'
+
+YARD::Rake::YardocTask.new do |t|
+  t.files   = ['lib/**/*.rb']
+  t.options = [
+    '--protected',
+    '--files', 'History.txt',
+    '--title', 'Contextify'
+  ]
+end
+
+task :docs => :yardoc
