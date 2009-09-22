d625b3dfee6f855932720ac7be926937f2a8427f
postmodern <postmodern.mod3@gmail.com>
2009-09-21 17:11:07 -0700

    Added the --quiet option to the yard task.

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -10,7 +10,8 @@ YARD::Rake::YardocTask.new do |t|
   t.options = [
     '--protected',
     '--files', 'History.txt',
-    '--title', 'Contextify'
+    '--title', 'Contextify',
+    '--quiet'
   ]
 end
 
