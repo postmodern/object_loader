21434da3b5b4f72af52b24dcaac361885273a79e
postmodern <postmodern.mod3@gmail.com>
2009-11-28 05:07:56 -0800

    Added PendingContext#each_class and PendingContext#each.

--- lib/contextify/pending_context.rb
+++ lib/contextify/pending_context.rb
@@ -1,6 +1,8 @@
 module Contextify
   class PendingContext
 
+    include Enumerable
+
     # The path being loaded
     attr_reader :path
 
@@ -19,6 +21,21 @@ module Contextify
     end
 
     #
+    # Iterates over each context name in the pending context.
+    #
+    # @yield [name]
+    #   The block will be passed each name of the pending context blocks.
+    #
+    # @yieldparam [String] name
+    #   The name of a pending context block.
+    #
+    def each_class(&block)
+      @blocks.each_key do |name|
+        block.call(Contextify.contexts[name])
+      end
+    end
+
+    #
     # Iterates over each block in the pending context.
     #
     # @yield [block]
@@ -29,6 +46,23 @@ module Contextify
     #
     def each_block(&block)
       @blocks.each_value(&block)
+    end
+
+    #
+    # Iterates over each context name and block in the pending context.
+    #
+    # @yield [name, block]
+    #   The block will be passed each pending context block and it's
+    #   context name.
+    #
+    # @yieldparam [String] name
+    #   The context name of the block.
+    #
+    # @yieldparam [Proc] block
+    #   A pending context block.
+    #
+    def each(&block)
+      @block.each(&block)
     end
 
   end
