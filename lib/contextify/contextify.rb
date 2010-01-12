e8eb6d67fb265543e16e24b5085b5e95c3ae3f97
postmodern <postmodern.mod3@gmail.com>
2010-01-12 01:18:32 -0800

    Added Contextify::ClassMethods.

--- lib/contextify/contextify.rb
+++ lib/contextify/contextify.rb
@@ -1,56 +1,10 @@
 require 'contextify/exceptions'
-require 'contextify/extensions'
+require 'contextify/class_methods'
 require 'contextify/pending_context'
 
 module Contextify
   def self.included(base)
-    base.module_eval do
-      #
-      # Contextifies the class.
-      #
-      # @param [Symbol, String] name
-      #   The context name to assign to the class.
-      #
-      def self.contextify(name)
-        name = name.to_s
-
-        Contextify.contexts[name] = self
-
-        meta_def(:context_name) { name }
-
-        meta_def(:load_context_block) do |path|
-          Contextify.load_block(name,path)
-        end
-
-        meta_def(:load_context) do |path,*args|
-          pending = Contextify.load_blocks(path)
-
-          context, block = pending.find do |name,block|
-            Contextify.contexts[name].ancestors.include?(self)
-          end
-
-          if (context && block)
-            obj = Contextify.contexts[name].new(*args)
-            obj.instance_eval(&block)
-            obj
-          end
-        end
-
-        # define the top-level context wrappers
-        Kernel.module_eval %{
-          def #{name}(*args,&block)
-            if (args.empty? && ::Contextify.is_pending?)
-              ::Contextify.pending.blocks[#{name.dump}] = block
-              return nil
-            else
-              new_context = #{self}.new(*args)
-              new_context.instance_eval(&block) if block
-              return new_context
-            end
-          end
-        }
-      end
-    end
+    base.extend ClassMethods
   end
 
   #
