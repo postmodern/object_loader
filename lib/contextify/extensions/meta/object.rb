8384390649ac97726456691613ca3292251464ac
postmodern <postmodern.mod3@gmail.com>
2010-02-15 21:22:18 -0800

    Removed RDoc :nodoc: option.

--- lib/contextify/extensions/meta/object.rb
+++ lib/contextify/extensions/meta/object.rb
@@ -1,5 +1,5 @@
 # metaprogramming assistant -- metaid.rb
-class Object # :nodoc:
+class Object
   # The hidden singleton lurks behind everyone
   def metaclass; class << self; self; end; end
   def meta_eval(&blk); metaclass.instance_eval(&blk); end
